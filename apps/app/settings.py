ad716f4fe51abc2af0f76a02a2919b86408e8004
jacob-dalek <jacobsenier@gmail.com>
2026-08-27 08:56:34 +0100

    Added htmx library to pip

--- apps/app/settings.py
+++ apps/app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'app',
     'crud',
+    'django_htmx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-
+    'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "./crud/index.html")
+    context = {"title": "Home"} 
+    return render(request, "./crud/index.html", context)
 
