8265802487d51bc4feb4b77f3ae9c502b96b3fa1
jacob-dalek <jacobsenier@gmail.com>
2026-08-27 13:04:55 +0100

    can serve static images need to fix doc-string :/

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -3,10 +3,6 @@ from django.conf import settings
 import os
 
 def index(request):
-    folder = os.listdir(os.path.join("C:/Users/jacob/Desktop/django-project/apps/crud/static/tink_images")) # string literal not great 
-
-    for files in folder:
-        print(files)
-
-    context = {"title": "Home"} 
+    tink_images = os.listdir(os.path.join("C:/Users/jacob/Desktop/django-project/apps/crud/static/tink_images")) # string literal not great 
+    context = {"title": "Home", "tink_images": tink_images}
     return render(request, "./crud/index.html", context)
