389b15f78e5c94ec77e9535060e8de0069102964
jacob-dalek <jacobsenier@gmail.com>
2026-08-27 12:46:38 +0100

    I have access to the static path via a string literal this needs to change ideally

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 def index(request):
+    folder = os.listdir(os.path.join("C:/Users/jacob/Desktop/django-project/apps/crud/static/tink_images")) # string literal not great 
+
+    for files in folder:
+        print(files)
+
     context = {"title": "Home"} 
     return render(request, "./crud/index.html", context)
