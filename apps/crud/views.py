6b7f25ef48ab8209fb72ce4bb29061792f51fea0
jacob-dalek <jacobsenier@gmail.com>
2026-08-27 13:52:11 +0100

    change of plans instead of serving static files i will have users do it and fetch data from the database

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -3,6 +3,5 @@ from django.conf import settings
 import os
 
 def index(request):
-    tink_images = os.listdir(os.path.join("C:/Users/jacob/Desktop/django-project/apps/crud/static/tink_images")) # string literal not great 
-    context = {"title": "Home", "tink_images": tink_images}
+    context = {"title": "Home"}
     return render(request, "./crud/index.html", context)
